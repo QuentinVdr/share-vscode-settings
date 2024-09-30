6f8b4f90bd4b8f81805b813c75f946a1089acd37
Quentin  VERDIER <quent1.verdier@gmail.com>
2024-09-30 16:41:44 +0200

    ♻️ Refactor ExtensionsDetailsList to accept props

--- src/components/extension/ExtensionsDetailsList/ExtensionsDetailsList.jsx
+++ src/components/extension/ExtensionsDetailsList/ExtensionsDetailsList.jsx
@@ -1,32 +1,22 @@
 import { useExtensionByIdsQueries } from '@hooks/reactQuery/queries/useExtensionsQueries';
-import { Button, Stack, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid2';
-import { useExtensionStore } from '@stores/extension/extensionStore';
-import { useState } from 'react';
+import PropTypes from 'prop-types';
 import { ExtensionDetailCard } from '../ExtensionDetailCard/ExtensionDetailCard';
-import ExtensionsSaveDialog from '../ExtensionsSaveDialog/ExtensionsSaveDialog';
 
-export default function ExtensionsDetailsList() {
-  const { extensionIds } = useExtensionStore();
+export default function ExtensionsDetailsList({ extensionIds }) {
   const extensionsQueries = useExtensionByIdsQueries(extensionIds);
-  const [open, setOpen] = useState(false);
 
   return (
-    <Stack direction="column" gap={4}>
-      <Stack direction="row" justifyContent="space-between" alignItems="center">
-        <Typography variant="h1">Extension details</Typography>
-        <Button variant="outlined" onClick={() => setOpen(true)}>
-          Share list
-        </Button>
-      </Stack>
-      <Grid container spacing={2}>
-        {extensionsQueries.map((query, index) => (
-          <Grid key={extensionIds[index]} size={{ xs: 12, md: 6, xl: 4 }}>
-            <ExtensionDetailCard extensionId={extensionIds[index]} extensionQuery={query} />
-          </Grid>
-        ))}
-      </Grid>
-      <ExtensionsSaveDialog open={open} handleClose={() => setOpen(false)} />
-    </Stack>
+    <Grid container spacing={2}>
+      {extensionsQueries.map((query, index) => (
+        <Grid key={extensionIds[index]} size={{ xs: 12, md: 6, xl: 4 }}>
+          <ExtensionDetailCard extensionId={extensionIds[index]} extensionQuery={query} />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
+
+ExtensionsDetailsList.propTypes = {
+  extensionIds: PropTypes.arrayOf(PropTypes.string).isRequired
+};
--- src/pages/ExtensionDetails/ExtensionDetails.jsx
+++ src/pages/ExtensionDetails/ExtensionDetails.jsx
@@ -1,10 +1,13 @@
 import ExtensionsDetailsList from '@components/extension/ExtensionsDetailsList/ExtensionsDetailsList';
+import ExtensionsSaveDialog from '@components/extension/ExtensionsSaveDialog/ExtensionsSaveDialog';
+import { Button, Stack, Typography } from '@mui/material';
 import { useExtensionStore } from '@stores/extension/extensionStore';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function ExtensionDetails() {
   const { extensionIds } = useExtensionStore();
+  console.log('🚀 ~ ExtensionDetails ~ extensionIds:', extensionIds);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -13,5 +16,18 @@ export default function ExtensionDetails() {
     }
   }, [extensionIds]);
 
-  return <ExtensionsDetailsList />;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Stack direction="column" gap={3}>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Typography variant="h1">Extension details</Typography>
+        <Button variant="outlined" onClick={() => setOpen(true)}>
+          Share list
+        </Button>
+      </Stack>
+      <ExtensionsDetailsList extensionIds={extensionIds} />
+      <ExtensionsSaveDialog open={open} handleClose={() => setOpen(false)} />
+    </Stack>
+  );
 }
