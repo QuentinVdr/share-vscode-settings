a2c6d7763f3364259c8edbe6e319c9a324e613f5
Quentin VERDIER <quent1.verdier@gmail.com>
2024-11-07 21:13:03 +0100

    ✨ Update HTML title and add meta tags for SEO; correct Navbar title casing

--- src/components/global/Navbar/Navbar.jsx
+++ src/components/global/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <AppBar className={styles.navbar} position="sticky">
       <Stack direction="row" gap={2} alignItems="center">
         <Link to="/" className={styles.logo}>
-          <Typography variant="h1">Share vscode settings</Typography>
+          <Typography variant="h1">Share VSCode settings</Typography>
         </Link>
       </Stack>
     </AppBar>
